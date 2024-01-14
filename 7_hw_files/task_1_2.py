f052c4f43188d4309275b9ba91f635f514679e65
Alexander Bakshaev <shuriknt@gmail.com>
2024-01-14 23:01:43 +0500

    fix task_1_2

--- 7_hw_files/task_1_2.py
+++ 7_hw_files/task_1_2.py
@@ -4,17 +4,25 @@ from pprint import pprint
 def creating_dict():
     with open('cook_book.txt', 'r') as file:
         cook_book = {}
+        dish = None
         for line in file:
-            dish = line.rstrip()
-            ingredients_count = int(file.readline().strip())
-            cook_book[dish] = [0] * ingredients_count
-            for item in range(ingredients_count):
-                string_ingredient = file.readline().strip().split(' | ')
-                cook_book[dish][item] = {'ingredient_name': string_ingredient[0],
-                                       'quantity': int(string_ingredient[1]),
-                                       'measure': string_ingredient[2]}
-            file.readline()
-
+            line = line.strip()
+            if not line:
+                continue
+            if dish is None:
+                dish = line
+                cook_book[dish] = []
+            else:
+                ingredients_count = int(line)
+                for _ in range(ingredients_count):
+                    string_ingredient = file.readline().strip().split(' | ')
+                    ingredient = {
+                        'ingredient_name': string_ingredient[0],
+                        'quantity': int(string_ingredient[1]),
+                        'measure': string_ingredient[2]
+                    }
+                    cook_book[dish].append(ingredient)
+                dish = None
     return cook_book
 
 
@@ -22,8 +30,8 @@ pprint(creating_dict(), width=100)
 
 
 def get_shop_list_by_dishes(dishes: list, person_count: int):
-    result_dict = {}
     cook_book = creating_dict()
+    result_dict = {}
     for dish in dishes:
         for value in cook_book[dish]:
             ingredient = value['ingredient_name']
