47236b2983396f6c96dcb5db15c252ee3035291f
Alexander Bakshaev <shuriknt@gmail.com>
2024-03-29 15:27:16 +0500

    update 1

--- prof_python_hw_1/application/salary.py
+++ prof_python_hw_1/application/salary.py
@@ -2,7 +2,6 @@ import numpy as np
 
 def calculate_salary():
     print("Вычисление оклада сотрудников...")
-    # Пример использования numpy для расчета среднего значения
     data = [2000, 2000, 3000, 4000, 5000]
     mean_salary = np.mean(data)
     print("Средний оклад:", mean_salary)
